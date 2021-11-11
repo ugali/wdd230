3c1c43a88c0138f6f76f099f58d8cfe4956751ca
WDD230-BYUI <udaygali07@gmail.com>
2021-11-11 11:47:52 -0600

    birthplace and dates are added to the prophets cards

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -5,21 +5,26 @@ fetch(requestURL)
 		return response.json();
 	})
 	.then(function (jsonObject) {
-        
-        console.log(jsonObject);
-        const prophets = jsonObject['prophets'];
-        
-        for (let i = 0; i < prophets.length; i++) {
-            let cards = document.createElement('section');
-            cards.setAttribute("class", "card");
-            let h2 = document.createElement('h2');
-            let image = document.createElement('IMG');
-            let url = prophets[i].imageurl;
-            image.setAttribute("src", url);
-            h2.textContent = `${prophets[i].name}  ${prophets[i].lastname}`;
-            cards.appendChild(image);
-            cards.appendChild(h2);
-            document.querySelector('div.container').appendChild(cards); 
-        }
-	});
+		console.log(jsonObject);
+		const prophets = jsonObject['prophets'];
 
+		for (let i = 0; i < prophets.length; i++) {
+			let cards = document.createElement('section');
+			cards.setAttribute('class', 'card');
+			let h2 = document.createElement('h2');
+			let image = document.createElement('IMG');
+			let url = prophets[i].imageurl;
+			image.setAttribute('src', url);
+			h2.textContent = `${prophets[i].name}  ${prophets[i].lastname}`;
+			let birthDate = document.createElement('p');
+			let birthPlace = document.createElement('p');
+			birthDate.innerHTML = `<b>Birthdate:  </b> + ${prophets[i].birthdate}`;
+			birthPlace.innerHTML = `<b>Birthplace: </b> + ${prophets[i].birthplace}`;
+
+			cards.appendChild(image);
+			cards.appendChild(h2);
+			cards.appendChild(birthDate);
+			cards.appendChild(birthPlace);
+			document.querySelector('div.container').appendChild(cards);
+		}
+	});
